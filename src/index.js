a27dcc68771555fa3a6eb30364416853c6078e55
Sergey Sova <mail@sergeysova.com>
2019-02-12 18:11:12 +0300

    chore: setup developer tools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { Button, ThemeProvider } from "mineral-ui";
-import * as serviceWorker from "./serviceWorker";
+import React from "react"
+import ReactDom from "react-dom"
+import { Button, ThemeProvider } from "mineral-ui"
+import { BrowserRouter } from "react-router-dom"
+import * as serviceWorker from "./service-worker"
 
-const App = () => (
-  <ThemeProvider>
-    <Button>Hello world</Button>
-  </ThemeProvider>
-);
+const root = document.getElementById("root")
 
-ReactDom.render(<App />, document.getElementById("root"));
+const render = () =>
+  ReactDom.render(
+    <BrowserRouter>
+      <ThemeProvider>
+        <Button>Hello world</Button>
+      </ThemeProvider>
+    </BrowserRouter>,
+    root,
+  )
+
+if (module.hot) {
+  module.hot.accept("./app", render)
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+render()
