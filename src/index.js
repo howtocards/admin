3c17c36ce0e11d7dee15fb9fff69ec386693bf3b
Sergey Sova <mail@sergeysova.com>
2019-02-12 17:39:40 +0300

    feat: initialize with custom setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import { Button, ThemeProvider } from "mineral-ui";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <ThemeProvider>
+    <Button>Hello world</Button>
+  </ThemeProvider>
+);
+
+ReactDom.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
