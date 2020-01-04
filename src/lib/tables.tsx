637d46d918aef6dc82fe9709bf162c5fd565f6cb
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2020-01-05 01:51:52 +0300

    feat(users): add search to table

---
+++ src/lib/tables.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Input, Button, Icon } from 'antd';
+import Highlighter from 'react-highlight-words';
+
+type FilterDropdown = {
+  setSelectedKeys: (keys: string[]) => string;
+  selectedKeys: string[];
+  confirm: () => void;
+  clearFilters: () => void;
+};
+
+export const addColumnSearch = (dataIndex: string, ref: any) => ({
+  filterDropdown: ({
+    setSelectedKeys,
+    selectedKeys,
+    confirm,
+  }: FilterDropdown) => (
+    <div style={{ padding: 8 }}>
+      <Input
+        ref={ref}
+        placeholder={`Search ${dataIndex}`}
+        value={selectedKeys[0]}
+        onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+        onPressEnter={confirm}
+        style={{ width: 188, marginBottom: 8, display: 'block' }}
+      />
+      <Button
+        type="primary"
+        onClick={confirm}
+        icon="search"
+        size="small"
+        style={{ width: 90, marginRight: 8 }}
+      >
+        Search
+      </Button>
+    </div>
+  ),
+  filterIcon: (filtered: boolean) => (
+    <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+  ),
+
+  render: (text: string) => {
+    console.log('ref', ref.current.props.value);
+
+    return (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069' }}
+        searchWords={[ref.current.props.value]}
+        autoEscape
+        textToHighlight={text && text.toString()}
+      />
+    );
+  },
+});
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,35 +3,46 @@ import styled from 'styled-components';
 import { Table } from 'antd';
 import { useStore } from 'effector-react';
 
+import { addColumnSearch } from 'lib/tables';
 import { pageMounted, $usersLits } from './model';
 
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'User',
-    dataIndex: 'userName',
-    key: 'userName',
-  },
-  {
-    title: 'Name',
-    dataIndex: 'displayName',
-    key: 'displayName',
-  },
-  {
-    title: 'E-mail',
-    dataIndex: 'email',
-    key: 'email',
-  },
-];
-
 export const UsersPage = () => {
+  const userNameRef = React.createRef();
+  const displayNameRef = React.createRef();
+  const emailRef = React.createRef();
+
   React.useEffect(() => pageMounted(), []);
+
   const usersLits = useStore($usersLits);
 
+  const columns = React.useMemo(
+    () => [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'User',
+        dataIndex: 'userName',
+        key: 'userName',
+        ...addColumnSearch('userName', userNameRef),
+      },
+      {
+        title: 'Name',
+        dataIndex: 'displayName',
+        key: 'displayName',
+        ...addColumnSearch('displayName', displayNameRef),
+      },
+      {
+        title: 'E-mail',
+        dataIndex: 'email',
+        key: 'email',
+        ...addColumnSearch('email', emailRef),
+      },
+    ],
+    [],
+  );
   return (
     <UsersContainer>
       <Table
